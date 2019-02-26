0929ae5f536660cc1601bc4310a3e1aacc13e3d9
Naveen Kusakula <naveenkusakula@TE599.WPA.Dal.Ca>
2019-02-26 19:30:08 -0400

    adpater

--- DesignPatterns/src/main/java/practicum/Adapter/Adapter.java
+++ DesignPatterns/src/main/java/practicum/Adapter/Adapter.java
@@ -1,9 +1,10 @@
 package practicum.Adapter;
 
 public class Adapter extends ExternalAccountsPayableSystem implements IAccountsPayable {
-
+	
 	public void printCheques() {
 		printChecks();
 	}
 
 }
+	
