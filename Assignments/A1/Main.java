6f4948d4383bd6f82d616b6020896cfda8504d86
Robert Hawkey <rhawkey@pletter.com>
2019-05-12 13:16:10 -0300

    [Cleanup] Removing previous semesters assignment code.

--- Assignments/A1/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main
-{
-	public static void main(String[] args)
-	{
-		IOrderProcessor p = new OrderProcessor();
-		p.RunUnitTests();
-	}
-}
--- Assignments/A3/IWarehouseFactory.java
+++
@@ -1,9 +0,0 @@
-public interface IWarehouseFactory
-{
-	// Robots and batteries
-	public IBattery makeBattery(int capacity);
-	public IRobot makeRobot(int batteryCapacity);
-	public IRobot makeRobotWithBatteryPack(int batteryCapacity, int packCapacity);
-	// States
-	// .. You must complete the interface with methods to instantiate your state objects.
-}
