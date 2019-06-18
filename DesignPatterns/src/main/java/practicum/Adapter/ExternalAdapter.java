a43b5b1db112b0a4813f5e5c4c7cc089ab061ce5
DragonRaider996 <anant.130410116086@gmail.com>
2019-06-18 19:13:39 -0300

    Changes

--- DesignPatterns/src/main/java/practicum/Adapter/ExternalAdapter.java
+++ DesignPatterns/src/main/java/practicum/Adapter/ExternalAdapter.java
@@ -1,7 +1,6 @@
 package practicum.Adapter;
 
 public class ExternalAdapter extends ExternalAccountsPayableSystem implements IAccountsPayable{
-    IAccountingFactory accountingFactory = new AccountingFactory();
     public void printCheques() {
         super.printChecks();
     }
